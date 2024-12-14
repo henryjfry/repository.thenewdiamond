c2959a4a97cb650016e83255d332ef0deebfc192
Philip Shaw <henry.j.fry@gmail.com>
2024-12-14 11:19:57 +0000

    2.0.0.5

--- plugin.video.fenlight/resources/lib/modules/sources.py
+++ plugin.video.fenlight/resources/lib/modules/sources.py
@@ -12,8 +12,6 @@ from modules.source_utils import get_cache_expiry, make_alias_dict
 from modules.utils import clean_file_name, string_to_float, safe_string, remove_accents, get_datetime, append_module_to_syspath, manual_function_import, manual_module_import
 # logger = kodi_utils.logger
 
-import xbmc
-
 get_icon, notification, sleep, xbmc_monitor = kodi_utils.get_icon, kodi_utils.notification, kodi_utils.sleep, kodi_utils.xbmc_monitor
 select_dialog, confirm_dialog, close_all_dialog = kodi_utils.select_dialog, kodi_utils.confirm_dialog, kodi_utils.close_all_dialog
 show_busy_dialog, hide_busy_dialog, xbmc_player = kodi_utils.show_busy_dialog, kodi_utils.hide_busy_dialog, kodi_utils.xbmc_player
@@ -28,17 +26,19 @@ autoplay_next_episode, autoscrape_next_episode, limit_resolve = settings.autopla
 auto_episode_group, preferred_autoplay, debrid_enabled = settings.auto_episode_group, settings.preferred_autoplay, debrid.debrid_enabled
 get_progress_status_movie, get_bookmarks_movie, erase_bookmark = watched_status.get_progress_status_movie, watched_status.get_bookmarks_movie, watched_status.erase_bookmark
 get_progress_status_episode, get_bookmarks_episode = watched_status.get_progress_status_episode, watched_status.get_bookmarks_episode
-internal_include_list = ['easynews', 'pm_cloud', 'rd_cloud', 'ad_cloud']
+internal_include_list = ['easynews', 'rd_cloud', 'pm_cloud', 'ad_cloud', 'oc_cloud', 'tb_cloud']
 external_exclude_list = ['easynews', 'gdrive', 'library', 'filepursuit', 'plexshare']
 sd_check = ('SD', 'CAM', 'TELE', 'SYNC')
 rd_info, pm_info, ad_info = ('apis.real_debrid_api', 'RealDebridAPI'), ('apis.premiumize_api', 'PremiumizeAPI'), ('apis.alldebrid_api', 'AllDebridAPI')
+oc_info, ed_info, tb_info = ('apis.offcloud_api', 'OffcloudAPI'), ('apis.easydebrid_api', 'EasyDebridAPI'), ('apis.torbox_api', 'TorBoxAPI')
 debrids = {'Real-Debrid': rd_info, 'rd_cloud': rd_info, 'rd_browse': rd_info, 'Premiumize.me': pm_info, 'pm_cloud': pm_info, 'pm_browse': pm_info,
-			'AllDebrid': ad_info, 'ad_cloud': ad_info, 'ad_browse': ad_info}
-debrid_providers = ('Real-Debrid', 'Premiumize.me', 'AllDebrid')
-debrid_token_dict = {'Real-Debrid': 'rd.token' , 'Premiumize.me': 'pm.token' , 'AllDebrid': 'ad.token'}
+			'AllDebrid': ad_info, 'ad_cloud': ad_info, 'ad_browse': ad_info, 'Offcloud': oc_info, 'oc_cloud': oc_info, 'oc_browse': oc_info,
+			'EasyDebrid': ed_info, 'ed_cloud': ed_info, 'ed_browse': ed_info, 'TorBox': tb_info, 'tb_cloud': tb_info, 'tb_browse': tb_info}
+debrid_providers = ('Real-Debrid', 'Premiumize.me', 'AllDebrid', 'Offcloud', 'EasyDebrid', 'TorBox')
+debrid_token_dict = {'Real-Debrid': 'rd.token', 'Premiumize.me': 'pm.token', 'AllDebrid': 'ad.token', 'Offcloud': 'oc.token', 'EasyDebrid': 'ed.token', 'TorBox': 'tb.token'}
 quality_ranks = {'4K': 1, '1080p': 2, '720p': 3, 'SD': 4, 'SCR': 5, 'CAM': 5, 'TELE': 5}
-cloud_scrapers, folder_scrapers = ('rd_cloud', 'pm_cloud', 'ad_cloud'), ('folder1', 'folder2', 'folder3', 'folder4', 'folder5')
-default_internal_scrapers = ('easynews', 'rd_cloud', 'pm_cloud', 'ad_cloud', 'folders')
+cloud_scrapers, folder_scrapers = ('rd_cloud', 'pm_cloud', 'ad_cloud', 'oc_cloud', 'tb_cloud'), ('folder1', 'folder2', 'folder3', 'folder4', 'folder5')
+default_internal_scrapers = ('easynews', 'rd_cloud', 'pm_cloud', 'ad_cloud', 'oc_cloud', 'tb_cloud', 'folders')
 main_line = '%s[CR]%s[CR]%s'
 int_window_prop = 'fenlight.internal_results.%s'
 scraper_timeout = 25
@@ -571,7 +571,8 @@ class Sources():
 
 	def debridPacks(self, debrid_provider, name, magnet_url, info_hash, download=False):
 		show_busy_dialog()
-		debrid_info = {'Real-Debrid': 'rd_browse', 'Premiumize.me': 'pm_browse', 'AllDebrid': 'ad_browse'}[debrid_provider]
+		debrid_info = {'Real-Debrid': 'rd_browse', 'Premiumize.me': 'pm_browse', 'AllDebrid': 'ad_browse',
+						'Offcloud': 'oc_browse', 'EasyDebrid': 'ed_browse', 'TorBox': 'tb_browse'}[debrid_provider]
 		debrid_function = self.debrid_importer(debrid_info)
 		try: debrid_files = debrid_function().display_magnet_pack(magnet_url, info_hash)
 		except: debrid_files = None
@@ -832,7 +833,7 @@ class Sources():
 				url = resolve_easynews({'url_dl': url_dl, 'play': 'false'})
 			else:
 				debrid_function = self.debrid_importer(scrape_provider)
-				if any(i in scrape_provider for i in ('rd_', 'ad_')):
+				if any(i in scrape_provider for i in ('rd_', 'ad_', 'tb_')):
 					url = debrid_function().unrestrict_link(item_id)
 				else:
 					if '_cloud' in scrape_provider: item_id = debrid_function().get_item_details(item_id)['link']
--- plugin.video.fenlight/resources/lib/scrapers/tb_cloud.py
+++ plugin.video.fenlight/resources/lib/scrapers/tb_cloud.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
+# Thanks to kodifitzwell for allowing me to borrow his code
 from threading import Thread
-from apis.alldebrid_api import AllDebridAPI
+from apis.torbox_api import TorBoxAPI
 from modules import source_utils
 from modules.utils import clean_file_name, normalize
 from modules.settings import enabled_debrids_check, filter_by_name
 # from modules.kodi_utils import logger
 
+
 internal_results, check_title, clean_title, get_aliases_titles = source_utils.internal_results, source_utils.check_title, source_utils.clean_title, source_utils.get_aliases_titles
 get_file_info, release_info_format, seas_ep_filter = source_utils.get_file_info, source_utils.release_info_format, source_utils.seas_ep_filter
-AllDebrid = AllDebridAPI()
+TorBox = TorBoxAPI()
 extensions = source_utils.supported_video_extensions()
 
 class source:
 	def __init__(self):
-		self.scrape_provider = 'ad_cloud'
+		self.scrape_provider = 'tb_cloud'
 		self.sources = []
 
 	def results(self, info):
 		try:
-			if not enabled_debrids_check('ad'): return internal_results(self.scrape_provider, self.sources)
+			if not enabled_debrids_check('tb'): return internal_results(self.scrape_provider, self.sources)
 			self.folder_results, self.scrape_results = [], []
 			filter_title = filter_by_name(self.scrape_provider)
 			self.media_type, title = info.get('media_type'), info.get('title')
@@ -31,10 +33,10 @@ class source:
 			def _process():
 				for item in self.scrape_results:
 					try:
-						file_name = normalize(item['filename'])
+						file_name = normalize(item['name'])
 						if filter_title and not check_title(title, file_name, self.aliases, self.year, self.season, self.episode): continue
 						display_name = clean_file_name(file_name).replace('html', ' ').replace('+', ' ').replace('-', ' ')
-						file_dl, size = item['link'], round(float(int(item['size']))/1073741824, 2)
+						file_dl, size = '%d,%d' % (int(item['folder_id']), item['id']), round(float(int(item['size']))/1073741824, 2)
 						video_quality, details = get_file_info(name_info=release_info_format(file_name))
 						source_item = {'name': file_name, 'display_name': display_name, 'quality': video_quality, 'size': size, 'size_label': '%.2f GB' % size,
 									'extraInfo': details, 'url_dl': file_dl, 'id': file_dl, 'downloads': False, 'direct': True, 'source': self.scrape_provider,
@@ -44,44 +46,31 @@ class source:
 			self.sources = list(_process())
 		except Exception as e:
 			from modules.kodi_utils import logger
-			logger('alldebrid scraper Exception', str(e))
+			logger('torbox scraper Exception', str(e))
 		internal_results(self.scrape_provider, self.sources)
 		return self.sources
 
 	def _scrape_cloud(self):
 		try:
-			try:
-				my_cloud_files = AllDebrid.user_cloud()['magnets']
-				my_cloud_files = [i for i in my_cloud_files if i['statusCode'] == 4]
-			except: return self.sources
-			threads = []
-			results_append = self.folder_results.append
-			append = threads.append
-			year_query_list = self._year_query_list()
-			for item in my_cloud_files:
-				normalized = normalize(item['filename'])
-				folder_name = clean_title(normalized)
-				if not folder_name: results_append(item)
-				elif not self.folder_query in folder_name: continue
-				else:
-					if self.media_type == 'movie' and not any(x in normalized for x in year_query_list): continue
-					results_append(item)
-			if not self.folder_results: return self.sources
-			for i in self.folder_results: append(Thread(target=self._scrape_folders, args=(i,)))
-			[i.start() for i in threads]
-			[i.join() for i in threads]
-		except: pass
-
-	def _scrape_folders(self, folder_info):
-		try:
-			links = folder_info['links']
 			append = self.scrape_results.append
-			links = [i for i in links if i['filename'].lower().endswith(tuple(extensions))]
-			for item in links:
-				normalized = normalize(item['filename'])
-				if self.media_type == 'episode' and not seas_ep_filter(self.season, self.episode, normalized): continue
-				append(item)
+			year_query_list = self._year_query_list()
+			try: my_cloud_files = TorBox.user_cloud()
+			except: return self.sources
+			for item in my_cloud_files['data']:
+				if not item['download_finished']: continue
+				if not self.folder_query in clean_title(normalize(item['name'])): continue
+				folder_id = item['id']
+				for file in item['files']:
+					if not file['short_name'].endswith(tuple(extensions)): continue
+					normalized = normalize(file['short_name'])
+					folder_name = clean_title(normalized)
+					if self.media_type == 'movie':
+						if not any(x in normalized for x in year_query_list): continue
+					elif not seas_ep_filter(self.season, self.episode, normalized): continue
+					file['folder_id'] = folder_id
+					append(file)
 		except: return
 
 	def _year_query_list(self):
 		return (str(self.year), str(self.year+1), str(self.year-1))
+
